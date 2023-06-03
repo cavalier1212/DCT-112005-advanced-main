30a55caf3fc8cafa5a2ee01062af76d32603ba4b
cavalier1212 <cavalier121@gmail.com>
2023-06-03 14:44:19 +0800

    feat: FormGroup 和 FormArray 調整表單的模型結構

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,17 +13,34 @@ export class RegisterComponent {
 
   formBuilder = inject(FormBuilder);
 
+  formSubmitted = false;
+
   form = this.formBuilder.group({
-    firstName: 'Alix',
-    lastName: 'Dai',
-    email: '',
-    password: '',
-    confirmPassword: ''
+    firstName: this.formBuilder.control('Mike'),
+    lastName: this.formBuilder.control('Huang'),
+    email: this.formBuilder.control(''),
+    password: this.formBuilder.group({
+      password: this.formBuilder.control(''),
+      confirmPassword: this.formBuilder.control(''),
+    }),
+    skills: this.formBuilder.array([
+      this.formBuilder.control('HTML'),
+      this.formBuilder.control('CSS'),
+      this.formBuilder.control('Javascript'),
+      this.formBuilder.control('Angular'),
+    ])
+
   })
 
   register() {
+    // controls 內的 controls
+    this.form.controls.skills.controls
+
+
+    let fieldName = 'firstName';
+    this.formSubmitted = true;
     console.log(this.form.value);
-    console.log('firstName (form.get)', this.form.get('firstName')?.value);
+    console.log('firstName (form.get)', this.form.get(fieldName)?.value);
     console.log('firstName (form.controls)', this.form.controls.firstName.value);
   }
 }
