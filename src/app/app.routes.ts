ac89d5b884776755e62313188c141fbb623c5c43
cavalier1212 <cavalier121@gmail.com>
2023-06-03 13:31:04 +0800

    feat: 新增register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -36,6 +36,10 @@ export const routes: Routes = [
     ]
   },
   {
+    path: 'register',
+    loadComponent: () => import('./register/register.component').then(c => c.RegisterComponent)
+  },
+  {
     path: 'login',
     loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
   }
