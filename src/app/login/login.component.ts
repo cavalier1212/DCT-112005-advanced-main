112817e6b0f07dd2fc0cf13e40f87d83247f3016
cavalier1212 <cavalier121@gmail.com>
2023-06-03 11:55:04 +0800

    feat: template driven form加入驗證&取的資料

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule, NgForm, NgModel, NgModelGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +10,48 @@ import { FormsModule, NgForm } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(NgForm) form: NgForm | undefined;
+  @ViewChild('tEmail') email!: NgModel
+  @ViewChild('tGroup1') group1!: NgModelGroup
 
   router = inject(Router)
 
+  ngOnInit(): void {
+    // this.email?.valueChanges?.subscribe(value => {
+    //   console.log('value: ', value);
+    // })
+  }
+
+  ngAfterViewInit(): void {
+    // template driven form 要在ngAfterViewInit後才有實體
+    // 不能用在ngOnInit
+    this.email?.valueChanges?.subscribe(value => {
+      console.log('value: ', value);
+    })
+  }
+
   login(userForm: any){
+    // delete userForm.agreement
     console.log('userForm: ', userForm);
+    console.log('form: ', this.form);
     console.log('login ....');
     this.router.navigate(['/'])
   }
 
+  loginSubmit(event: any, loginUser: any) {
+    console.log('loginUser: ', loginUser);
+    console.log('event: ', event);
+    console.log('this.form: ', this.form);
+    console.log('this.email: ', this.email);
+    if(this.form?.invalid) {
+      return
+    }
+    console.log('this.group1.value: ', this.group1.value);
+    // this.form?.reset();
+    // 可以指定reset欄位
+    // resetForm 才能reset submitted status
+    this.form?.resetForm({email: 'qwe@222d.sd'})
+  }
 }
