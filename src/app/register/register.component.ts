55b936423f96020e7a6cd67aed6026846b68926f
cavalier1212 <cavalier121@gmail.com>
2023-06-03 13:48:25 +0800

    feat : Model-driven form 建立表單模型並繫結

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule], // ReactiveFormsModule
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
 
+  formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    firstName: 'Alix',
+    lastName: 'Dai',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  register() {
+    console.log(this.form.value);
+    console.log('firstName (form.get)', this.form.get('firstName')?.value);
+    console.log('firstName (form.controls)', this.form.controls.firstName.value);
+  }
 }
